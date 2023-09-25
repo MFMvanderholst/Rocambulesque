88bc857f25dc9ac14bf4815b50dcaaae73ed5b10
Steff Brouwer <sbrouwer05@gmail.com>
2023-09-25 13:24:57 +0200

    reserveer pagina aangemaakt

---
+++ Rocambulesque/resources/views/reserveren.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reserveren.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="navbar">
+        <div class="navbar">
+            <div class="logo"></div>
+            <!-- <div class="loginicon"></div> -->
+                <button class="loginbutton" type="button" href="welcome.blade.php">Login</button>
+    </div>
+
+    <div class="background-image"></div>
+</body>
+</html>
