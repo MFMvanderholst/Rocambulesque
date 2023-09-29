931dfc4af2969033cc061118ec441f63132db82b
ramos013 <334020@student.mboutrecht.nl>
2023-09-29 14:09:53 +0200

    home-page

--- Rocambulesque/resources/views/welcome.blade.php
+++ Rocambulesque/resources/views/welcome.blade.php
@@ -10,16 +10,16 @@
 
 <body>
     <div class="homepage">
-        
 
-    <header>
-        <div class="logo">
-            <img src="images/restaurant-logo.png" alt="Restaurant Logo">
-        </div>
-        <div class="buttons">
-            <button class="logInBtn" onclick="location.href='loginPage'">Log In</button>
-            <button class="svg-button">
-            <svg viewBox="0 0 68 63" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+        <header>
+            <div class="logo">
+                <img src="images/restaurant-logo.png" alt="Restaurant Logo">
+            </div>
+            <div class="buttons">
+                <button class="logInBtn" onclick="location.href='loginPage'">Log In</button>
+                <button class="svg-button">
+                    <svg viewBox="0 0 68 63" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <g filter="url(#filter0_d_4_9)">
                             <path d="M34 27.4141C29.875 27.4141 26.3438 26.0988 23.4062 23.4683C20.4688 20.8377 19 17.6754 19 13.9814C19 10.2875 20.4688 7.12519 23.4062 4.49462C26.3438 1.86405 29.875 0.548767 34 0.548767C38.125 0.548767 41.6563 1.86405 44.5938 4.49462C47.5313 7.12519 49 10.2875 49 13.9814C49 17.6754 47.5313 20.8377 44.5938 23.4683C41.6563 26.0988 38.125 27.4141 34 27.4141ZM4 54.2795V44.8766C4 42.9737 4.54688 41.2246 5.64063 39.6295C6.73438 38.0343 8.1875 36.817 10 35.9775C13.875 34.2424 17.8125 32.9411 21.8125 32.0736C25.8125 31.2061 29.875 30.7723 34 30.7723C38.125 30.7723 42.1875 31.2061 46.1875 32.0736C50.1875 32.9411 54.125 34.2424 58 35.9775C59.8125 36.817 61.2656 38.0343 62.3594 39.6295C63.4531 41.2246 64 42.9737 64 44.8766V54.2795H4ZM11.5 47.5632H56.5V44.8766C56.5 44.261 56.3281 43.7013 55.9844 43.1975C55.6406 42.6938 55.1875 42.302 54.625 42.0222C51.25 40.511 47.8438 39.3776 44.4062 38.622C40.9688 37.8664 37.5 37.4886 34 37.4886C30.5 37.4886 27.0313 37.8664 23.5938 38.622C20.1563 39.3776 16.75 40.511 13.375 42.0222C12.8125 42.302 12.3594 42.6938 12.0156 43.1975C11.6719 43.7013 11.5 44.261 11.5 44.8766V47.5632ZM34 20.6978C36.0625 20.6978 37.8281 20.0401 39.2969 18.7249C40.7656 17.4096 41.5 15.8284 41.5 13.9814C41.5 12.1345 40.7656 10.5533 39.2969 9.23803C37.8281 7.92275 36.0625 7.26511 34 7.26511C31.9375 7.26511 30.1719 7.92275 28.7031 9.23803C27.2344 10.5533 26.5 12.1345 26.5 13.9814C26.5 15.8284 27.2344 17.4096 28.7031 18.7249C30.1719 20.0401 31.9375 20.6978 34 20.6978Z" fill="#04BF9D" />
                         </g>
@@ -36,27 +36,35 @@
                             </filter>
                         </defs>
                     </svg>
-            </button>
-        </div>
-    </header>
+                </button>
+            </div>
+        </header>
 
 
         <main>
             <div class="background-image">
                 <div class="restaurant-photos">
-                    <div class="menu-reservation-container">
-                        <div class="menu-button">
-                            <a href="#">Menu</a>
-                        </div>
-                        <div class="reservation-button">
-                            <a href="#">Reservation</a>
-                        </div>
-                    </div>
+
                     <div class="photo-column">
-                        <img src="images/img1.png" alt="Restaurant Photo 1">
-                        <img src="images/img2.png" alt="Restaurant Photo 2">
-                        <img src="images/img3.png" alt="Restaurant Photo 3">
+                        <div class="photo1">
+                            <img src="images/img1.png" alt="Restaurant Photo 1">
+                        </div>
+                        <div class="photo2">
+                            <div class="menu-button">
+                                <a href="#">Menu</a>
+                            </div>
+                            <img src="images/img2.png" alt="Restaurant Photo 3">
+                        </div>
+                        <div class="photo3">
+                            <div class="reservation-button">
+                                <a href="#">Reservation</a>
+                            </div>
+                            <img src="images/img3.png" alt="Restaurant Photo 2">
+                        </div>
                     </div>
+
+
+
                 </div>
             </div>
         </main>
@@ -64,4 +72,3 @@
 </body>
 
 </html>
-
