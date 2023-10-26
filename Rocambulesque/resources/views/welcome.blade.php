46c4a0b0e1e1dc3bf54183a7359a9634be4239d1
Axel van_beek <336019@student.mboutrecht.nl>
2023-10-26 13:11:10 +0200

    fixed an error with the login button

--- Rocambulesque/resources/views/welcome.blade.php
+++ Rocambulesque/resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
             <!-- <div class="buttons">
                 <button class="logInBtn" onclick="location.href='loginPage'">Log In
                 </button> -->
-                <div class="buttons">
+            <div class="buttons">
                 @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                     @auth
