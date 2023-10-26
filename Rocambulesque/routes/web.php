a46809b9dba98a030b4bfc7664869b7a760dbe33
Steff Brouwer <sbrouwer05@gmail.com>
2023-10-26 14:21:20 +0200

    routes update

--- Rocambulesque/routes/web.php
+++ Rocambulesque/routes/web.php
@@ -1,12 +1,12 @@
 <?php
-
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\CategoryController;
-use App\Http\Controllers\Admin\MenuController;
+// use App\Http\Controllers\Admin\MenuController;
 use App\Http\Controllers\Admin\ReservationController;
 use App\Http\Controllers\Admin\TableController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\RservationoverzichtController;
 use App\Http\Controllers\ReserveringklantController;
@@ -23,6 +23,37 @@ use App\Http\Controllers\WelcomeController;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+    Route::resource('/categories', CategoryController::class);
+    Route::resource('/menus', MenuController::class);
+    Route::resource('/reservation', ReservationController::class);
+    Route::resource('/tables', TableController::class);
+});
+
+require __DIR__ . '/auth.php';
+
+Route::get('/reserveren', function () {
+    return view('reserveren');
+});
+
+Route::get('/menu', function () {
+    return view('menu');
+});
 Route::get('/',[WelcomeController::class,'showWelcomePage']);
 Route::get('/welcome',[WelcomeController::class,'showWelcomePage']);
 Route::get('/reservationoverzicht',[RservationoverzichtController::class,'showReservationPage']);
