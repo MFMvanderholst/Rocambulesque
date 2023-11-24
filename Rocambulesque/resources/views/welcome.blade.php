3f5686087c0c0bda2e0c0478f995a9176c4edd07
Adham <337142@student.mboutrecht.nl>
2023-11-24 13:05:32 +0100

    Update front end

--- Rocambulesque/resources/views/welcome.blade.php
+++ Rocambulesque/resources/views/welcome.blade.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/home.css">
     <title>Restaurant Homepage</title>
 </head>
 
 <body>
-    <div class="homepage">
 
+<div class="bg"></div>
 
         <header>
             @include('navbar')
@@ -27,15 +27,17 @@
                         </div>
                         <div class="photo2">
                             <div class="content1">
-                                <h1>Welkom bij Restaurant Rocambolesque
-
-                                    Ontdek onze gloednieuwe eetervaring in Utrecht. Bij Restaurant Rocambolesque combineren we eigentijdse culinaire creativiteit met een warme en gastvrije sfeer.</h1>
+                                <h1>Bienvenue</h1> 
+                                <h2>
+                                bij Rocambolesque, waar we de charme en smaken van de Franse keuken naar jouw tafel brengen. Stap binnen en laat je meeslepen door de verfijnde ambiance en de heerlijke aroma's die onze keuken vullen.
+                                </h2>
                             </div>
                             <img src="images/img2.png" alt="Restaurant Photo 3">
                         </div>
                         <div class="photo3">
                             <div class="content2">
-                                <h1>Binnenkort kunt u al onze beschikbare arrangementen en seizoensmenu's bekijken, reserveringen beheren en zelfs speciale aanbiedingen voor feestdagen ontdekken. We kijken ernaar uit om u binnenkort te verwelkomen bij Rocambolesque!</h1>
+                                <h1>Ontdek</h1>
+                                <h2>de heerlijke finesse van onze gerechten, bereid met zorgvuldig geselecteerde ingrediënten die de essentie van Franse culinaire expertise weerspiegelen. Bekijk ons menu, reserveer je tafel en laat ons je meenemen op een reis door de rijke smaakpaletten van de Franse keuken.</h2>
                             </div>
                             <img src="images/img3.png" alt="Restaurant Photo 2">
                         </div>
