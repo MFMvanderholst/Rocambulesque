be1ee9a880b81ff3226b3ed4e417d5d04b16b0f7
Axel van_beek <336019@student.mboutrecht.nl>
2024-01-15 10:23:30 +0100

    admin dashboard changes

--- Rocambulesque/database/migrations/2023_10_09_113310_create_menus_table.php
+++ Rocambulesque/database/migrations/2023_10_09_113310_create_menus_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('description');
-            $table->string('image');
+            $table->string('image')->nullable()->default(null);
             $table->decimal('price', 10, 2);
             $table->unsignedBigInteger('menu_category_id');
             $table->unsignedBigInteger('dish_id');
